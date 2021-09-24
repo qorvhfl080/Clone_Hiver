dde8a705c354882b01fc7f4a9ee2f4e78897ca16
qorvhfl080 <86284854+qorvhfl080@users.noreply.github.com>
2021-09-24 12:39:50 +0900

    이메일로 회원가입(activity_sign_up_email) 화면 추가

---
+++ app/src/main/java/com/gecko/clone_hiver/activity/SignUpEmailActivity.kt
@@ -0,0 +1,26 @@
+package com.gecko.clone_hiver.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gecko.clone_hiver.databinding.ActivitySignUpEmailBinding
+
+class SignUpEmailActivity : BaseActivity() {
+
+    val binding by lazy { ActivitySignUpEmailBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        setupEvents()
+        setValues()
+
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
