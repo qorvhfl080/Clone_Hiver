deda6ddffabece0020ab5fb0cf3c9efcc0e06fcb
qorvhfl080 <86284854+qorvhfl080@users.noreply.github.com>
2021-09-30 19:10:00 +0900

    일부 수정

--- app/src/main/java/com/gecko/clone_hiver/adapters/BaseViewHolder.kt
+++ app/src/main/java/com/gecko/clone_hiver/adapters/BaseViewHolder.kt
@@ -5,7 +5,7 @@ import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.gecko.clone_hiver.utils.FontChanger
 
-class BaseViewHolder(context: Context, itemView: View) : RecyclerView.ViewHolder(itemView) {
+abstract class BaseViewHolder(context: Context, itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     init {
         FontChanger.setGlobalFont(context, itemView)
--- app/src/main/java/com/gecko/clone_hiver/adapters/recycler/ProductAdapter.kt
+++ app/src/main/java/com/gecko/clone_hiver/adapters/recycler/ProductAdapter.kt
@@ -3,12 +3,14 @@ package com.gecko.clone_hiver.adapters.recycler
 import android.content.Context
 import android.content.Intent
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.gecko.clone_hiver.R
 import com.gecko.clone_hiver.activity.ViewProductDetailActivity
+import com.gecko.clone_hiver.adapters.BaseViewHolder
 import com.gecko.clone_hiver.databinding.ProductListItemBinding
 import com.gecko.clone_hiver.databinding.SaleProductListItemBinding
 import com.gecko.clone_hiver.datas.ProductData
@@ -50,5 +52,19 @@ class ProductAdapter(val mContext: Context, val mList: List<ProductData>) : Recy
             productPrice.text = "${data.price}"
         }
     }
+
+//    inner class ProductViewHolder(view: View) : BaseViewHolder(mContext, view) {
+//        val productImg = binding.productImg
+//        val brandName = binding.brandTxt
+//        val productName = binding.productNameTxt
+//        val productPrice = binding.productPriceTxt
+//
+//        fun bind(context: Context, data: ProductData) {
+//            Glide.with(context).load(data.Img).into(productImg)
+//            brandName.text = data.brandName
+//            productName.text = data.productName
+//            productPrice.text = "${data.price}"
+//        }
+//    }
 
 }
